3932f88d1feac8daa20b09507563c9bfb6804eb0
Paco Gomez <paco.gomez.arnal@gmail.com>
2017-02-24 17:28:47 +0100

    TocaPelotas

--- src/Ventanas/VentanaPrincipal.java
+++ src/Ventanas/VentanaPrincipal.java
@@ -16,7 +16,7 @@ public class VentanaPrincipal extends JFrame {
 	 */
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(300, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
