2b8b1c18260f99b6ce3ad648ff8c22500e3d0e47
Paco Gomez <paco.gomez.arnal@gmail.com>
2017-02-24 17:29:27 +0100

    Soy el CTO

--- src/Ventanas/VentanaPrincipal.java
+++ src/Ventanas/VentanaPrincipal.java
@@ -16,7 +16,7 @@ public class VentanaPrincipal extends JFrame {
 	 */
 	public VentanaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
