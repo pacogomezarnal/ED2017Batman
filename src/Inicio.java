f56e01988904009480a0f5b333a698dc957873e2
Paco Gomez <paco.gomez.arnal@gmail.com>
2017-02-24 17:06:04 +0100

    Esqueleto de proyecto

---
+++ src/Inicio.java
@@ -0,0 +1,10 @@
+import Ventanas.VentanaPrincipal;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		VentanaPrincipal frame = new VentanaPrincipal();
+		frame.setVisible(true);
+	}
+
+}
